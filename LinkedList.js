ca699ab41fa291e5ed653752c7b5987e0a708e35
Ian <ian.culleton.92@gmail.com>
2022-08-21 10:35:04 -0400

    creates LinkedList class and adds readme material

---
+++ LinkedList.js
@@ -0,0 +1,42 @@
+class LinkedListNode {
+  constructor(value) {
+    this.value = value;
+    this.nextNode = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  addToTail(valueToAdd) {
+
+  }
+
+  addToHead(valueToAdd) {
+
+  }
+
+  removeFromTail() {
+
+  }
+
+  removeFromHead() {
+
+  }
+
+  insertAt(index, value) {
+
+  }
+
+  removeAt(index) {
+
+  }
+}
+
+module.exports = {
+  LinkedList,
+  LinkedListNode
+}
